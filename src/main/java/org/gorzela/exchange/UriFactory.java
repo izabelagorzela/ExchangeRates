39e15ec8d2df9f1c2956016440a0df2e766f5660
Iza <jza.q@wp.pl>
2018-10-31 08:19:50 +0100

    working in progress

--- src/main/java/org/gorzela/exchange/UriFactory.java
+++ src/main/java/org/gorzela/exchange/UriFactory.java
@@ -9,12 +9,16 @@ import java.net.URISyntaxException;
 @Component
 public class UriFactory {
 
+    public static final String PROTOCOL = "http";
+    public static final String HOST = "api.nbp.pl";
+    public static final String PATH = "/api/exchangerates/rates/c/";
+
     public URI getUri(String pathPart1, String pathPart2, String pathPart3) throws URISyntaxException {
 
         URIBuilder uriBuilder = new URIBuilder();
-        uriBuilder.setScheme("http");
-        uriBuilder.setHost("api.nbp.pl");
-        uriBuilder.setPath("/api/exchangerates/rates/c/" + pathPart1 + "/" + pathPart2 + "/" + pathPart3);
+        uriBuilder.setScheme(PROTOCOL);
+        uriBuilder.setHost(HOST);
+        uriBuilder.setPath(PATH + pathPart1 + "/" + pathPart2 + "/" + pathPart3);
         return uriBuilder.build();
     }
 }
