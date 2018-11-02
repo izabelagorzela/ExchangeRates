2a41e7f1b6c46d3673fba4ed941e2c71f55f732a
Iza <jza.q@wp.pl>
2018-11-02 09:21:20 +0100

    constants names changed

--- src/main/java/org/gorzela/exchange/ExchangeRatesDisplay.java
+++ src/main/java/org/gorzela/exchange/ExchangeRatesDisplay.java
@@ -11,19 +11,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class ExchangeRatesDisplay implements CommandLineRunner {
 
-    private static final int CURRENCY_CODE = 0;
-    private static final int DATE_FROM = 1;
-    private static final int DATE_TO = 2;
-    private static final int MINIMUM_ARGUMENTS_NUMBER = 3;
-    private static final int CALCULATION_VARIANT = 3;
+    private static final int ARG_MINIMUM_ARGUMENTS_NUMBER = 3;
+    private static final int ARG_CURRENCY_CODE = 0;
+    private static final int ARG_DATE_FROM = 1;
+    private static final int ARG_DATE_TO = 2;
+    private static final int ARG_CALCULATION_VARIANT = 3;
+
     private static final String MY_CALCULATION_VARIANT = "-m";
     private static final boolean MY_ALGORITHM = false;
-    private static final boolean APATCHE_ALGORITHM = true;
+    private static final boolean APACHE_ALGORITHM = true;
 
     @Autowired
     private NbpApiReader nbpApiReader;
 
-    private boolean algorithmVersion = APATCHE_ALGORITHM;
+    private boolean algorithmVersion = APACHE_ALGORITHM;
 
     @Override
     public void run(String[] args) throws Exception {
@@ -35,14 +36,14 @@ public class ExchangeRatesDisplay implements CommandLineRunner {
 
         log.info("Application is running...");
 
-        if(args.length < MINIMUM_ARGUMENTS_NUMBER) {
+        if(args.length < ARG_MINIMUM_ARGUMENTS_NUMBER) {
             System.out.println("No enought arguments");
         }
 
-        NBPResponse chargeData = nbpApiReader.getData(args[CURRENCY_CODE], args[DATE_FROM], args[DATE_TO]);
+        NBPResponse chargeData = nbpApiReader.getData(args[ARG_CURRENCY_CODE], args[ARG_DATE_FROM], args[ARG_DATE_TO]);
         if(chargeData != null) {
 
-            if(args.length > MINIMUM_ARGUMENTS_NUMBER && args[CALCULATION_VARIANT].equals(MY_CALCULATION_VARIANT)) {
+            if(args.length > ARG_MINIMUM_ARGUMENTS_NUMBER && args[ARG_CALCULATION_VARIANT].equals(MY_CALCULATION_VARIANT)) {
                 algorithmVersion = MY_ALGORITHM;
             }
 
@@ -85,7 +86,7 @@ public class ExchangeRatesDisplay implements CommandLineRunner {
 
     private void showResult(double arithmeticMean, double standardDeviation) {
 
-        if(algorithmVersion == APATCHE_ALGORITHM) {
+        if(algorithmVersion == APACHE_ALGORITHM) {
             System.out.println("Calculation performed with Apache library");
         }
         if(algorithmVersion == MY_ALGORITHM){
@@ -93,7 +94,5 @@ public class ExchangeRatesDisplay implements CommandLineRunner {
         }
         System.out.println(arithmeticMean + " : Purchase average");
         System.out.println(standardDeviation + " : Standard deviation");
-
-
     }
 }
